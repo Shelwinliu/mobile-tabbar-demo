f70a8955c774163f036d120b1aa6c841d5171eeb
Shelwinliu <1455483468@qq.com>
2021-07-24 15:23:23 +0800

    delete .history

--- .history/src/App_20210717161342.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="app">
-    div
-  </div>
-</template>
-
-<style>
-
-</style>
--- .history/src/App_20210717162145.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="app">
-    
-  </div>
-</template>
-
-<style>
-  @import "./assets/css/base.css"
-</style>
